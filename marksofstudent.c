67c53d556e8f2e7cefcc552559216e939645eb22
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-10-27 17:52:02 +0530

    Add files via upload

---
+++ marksofstudent.c
@@ -0,0 +1,13 @@
+// marks of the student in different subjects
+#include<stdio.h>
+void main()
+{
+	int phy,chem,maths,eng,tm; //Decleration
+	
+	phy = 60;
+    chem = 75;
+	maths = 65;
+ 	eng = 90;
+	
+	printf("total marks is %d",tm=phy+chem+maths+eng);	
+}
