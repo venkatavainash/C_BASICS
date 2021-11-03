f506a71a1128bb77d7333401cb72d82506d3dbed
venkatavainash <93145195+venkatavainash@users.noreply.github.com>
2021-11-03 12:29:37 +0530

    Add files via upload

---
+++ perimeterofSquare.c
@@ -0,0 +1,10 @@
+//perimeter of tghe Square
+#include<stdio.h>
+void main()
+{
+	int side,perimeter;
+	printf("enter the side of the Square :");
+	scanf("%d",&side);
+	perimeter = 4*side;
+	printf("perimeter of the Square with side %d is %d",side,perimeter);
+}
